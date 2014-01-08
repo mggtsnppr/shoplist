4453fd42a2424ba7397b4c66184e7a0ba878bf6e
Philip Dernovoy <dernovoy@ek.apress.ru>
2014-01-08 23:56:47 +0600

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shoplist::Application.config.secret_key_base = 'ed17a7632dcf36a616d6a5cdccd56c0dfc0bca201e3755dcdc236f9f2db7b214ad787e614f0e25daa486d85984b9bf05ed824ab7be6a9461101c7e95ab4f9c4f'
